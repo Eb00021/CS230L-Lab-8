afa627474b00608a3734339516c724a847c3aa5c
Eb00021 <143530245+Eb00021@users.noreply.github.com>
2023-10-31 22:31:53 +0000

    update app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Navbar from './Navbar';
+import Card from './Card';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>CS 230L</h1>
 	  <h2>Section - 002</h2>
 	  <p>WVU ID: 800355267</p>
 	  <p>Hi I am Ethan Boyd</p>
+      <Card />
     </div>
   );
 }
---
+++ src/card.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Card() {
+  return (
+    <div className="card-container">
+      <div className="card">
+        <h3>Card 1</h3>
+        <p>Content for Card 1.</p>
+      </div>
+      <div className="card">
+        <h3>Card 2</h3>
+        <p>Content for Card 2.</p>
+      </div>
+      <div className="card">
+        <h3>Card 3</h3>
+        <p>Content for Card 3.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
