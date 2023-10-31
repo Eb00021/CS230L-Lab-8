c5283d8d77036860e91ecb5ee114e81ed205e45e
Eb00021 <143530245+Eb00021@users.noreply.github.com>
2023-10-31 23:07:13 +0000

    bootstrap card and navbar

--- src/card.js
+++ src/card.js
@@ -2,18 +2,27 @@ import React from 'react';
 
 function Card() {
   return (
-    <div className="card-container">
+    <div className="card-deck">
       <div className="card">
-        <h3>Card 1</h3>
-        <p>Content for Card 1.</p>
+        <img src="card-image-1.jpg" className="card-img-top" alt="Card 1" />
+        <div className="card-body">
+          <h5 className="card-title">Card 1</h5>
+          <p className="card-text">Content for Card 1.</p>
+        </div>
       </div>
       <div className="card">
-        <h3>Card 2</h3>
-        <p>Content for Card 2.</p>
+        <img src="card-image-2.jpg" className="card-img-top" alt="Card 2" />
+        <div className="card-body">
+          <h5 className="card-title">Card 2</h5>
+          <p className="card-text">Content for Card 2.</p>
+        </div>
       </div>
       <div className="card">
-        <h3>Card 3</h3>
-        <p>Content for Card 3.</p>
+        <img src="card-image-3.jpg" className="card-img-top" alt="Card 3" />
+        <div className="card-body">
+          <h5 className="card-title">Card 3</h5>
+          <p className="card-text">Content for Card 3.</p>
+        </div>
       </div>
     </div>
   );
