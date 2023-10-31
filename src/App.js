69f89d784ac845ba6d6d793f3bd695075300c174
Eb00021 <143530245+Eb00021@users.noreply.github.com>
2023-10-31 22:35:43 +0000

    test2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import Navbar from './Navbar';
-import Card from './Card';
+import Navbar from './navbar.js';
+import Card from '/card.js';
 
 function App() {
   return (
