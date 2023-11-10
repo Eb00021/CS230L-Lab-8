b11339a78b5d3801441055e5420289c5d60f06de
Eb00021 <143530245+Eb00021@users.noreply.github.com>
2023-11-10 01:39:25 +0000

    lab7 update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './navbar';
-import Card from './card';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <BrowserRouter>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/card" element={<Card />}></Route>
+              <Route path="/contact" element={<Contact />}></Route>
+            </Routes>
+       </BrowserRouter>
+
       <h1>CS 230L</h1>
 	  <h2>Section - 002</h2>
 	  <p>WVU ID: 800355267</p>
 	  <p>Hi I am Ethan Boyd</p>
-      <Card />
     </div>
   );
 }
