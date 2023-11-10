4a3a4731b5af3f23ae3846d14bf5f5c487431f2b
Eb00021 <143530245+Eb00021@users.noreply.github.com>
2023-11-10 03:47:01 +0000

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './home.js';
 import Card from './card.js';
 import Contact from './contact.js';
@@ -9,18 +11,26 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-            <Navbar />
-            <Routes>
-              <Route path="./home.js" element={<Home />}></Route>
-              <Route path="./card.js" element={<Card />}></Route>
-              <Route path="./contact.js" element={<Contact />}></Route>
-            </Routes>
-       </BrowserRouter>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="/">CS 230L</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/card">Card</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar>
 
-      <h1>CS 230L</h1>
-	  <h2>Section - 002</h2>
-	  <p>WVU ID: 800355267</p>
-	  <p>Hi I am Ethan Boyd</p>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/card" element={<Card />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+
+        <h1>CS 230L</h1>
+        <h2>Section - 002</h2>
+        <p>WVU ID: 800355267</p>
+        <p>Hi, I am Ethan Boyd</p>
+      </BrowserRouter>
     </div>
   );
 }
