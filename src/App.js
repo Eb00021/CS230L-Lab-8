37b285f28e08741816ca93d23c74a0d2d81cb549
Eb00021 <143530245+Eb00021@users.noreply.github.com>
2023-11-10 03:31:23 +0000

    lab7 update4

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from './home.js';
 import Navbar from './navbar.js';
 import Card from './card.js';
+import Contact from './contact.js';
 
 function App() {
   return (
